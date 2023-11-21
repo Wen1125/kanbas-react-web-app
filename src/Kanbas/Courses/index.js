e5936ad5622e4a70ceef66ae3a88cdadf3be7d84
文莉李 <wenlili@wenlilidembp.mynetworksettings.com>
2023-11-20 21:30:56 -0800

    update

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -16,7 +16,9 @@ function Courses() {
     const [root, kanbas, courseName, id, screen] = pathname.split("/")
     const {courseId} = useParams()
     const [course, setCourse] = useState({});
-    const URL = "http://localhost:4000/api/courses";
+    const API_BASE = process.env.REACT_APP_API_BASE;
+    const URL = `${API_BASE}/courses`;
+    // const URL = "http://localhost:4000/api/courses";
     const findCourseById = async (courseId) => {
       const response = await axios.get(
         `${URL}/${courseId}`
