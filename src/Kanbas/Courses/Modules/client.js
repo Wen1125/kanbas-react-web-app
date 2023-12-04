9f794e4c8a31b7b1039a6ed3900e9d781d4d51be
文莉李 <wenlili@wenlilidembp.mynetworksettings.com>
2023-12-03 22:47:48 -0800

    a6

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -3,7 +3,6 @@ import axios from "axios";
 // const COURSES_URL = "http://localhost:4000/api/courses";
 // const MODULES_URL = "http://localhost:4000/api/modules";
 // const MODULES_URL = "https://kanbas-node-server-app-2b43.onrender.com/api/modules";
-// const API_BASE = process.env.REACT_APP_API_BASE;
 // const MODULES_URL = `${API_BASE}/modules`;
 const API_BASE = process.env.REACT_APP_API_BASE;
 const COURSES_URL = `${API_BASE}/courses`;
